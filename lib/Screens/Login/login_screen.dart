fd5bf48acd5992f09405c9f81697552289ff0fc2
Ronel Tano <ronel.tano98@gmail.com>
2022-06-08 12:14:17 +0800

    changed/modified login screen and signup screen

--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -1,11 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Login/components/body.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../Signup/signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
+  LoginScreen({
+    Key key,
+  }) : super(key: key);
+
+  final TextEditingController _emailCtrl = TextEditingController();
+  final TextEditingController _passwordCtrl = TextEditingController();
+  final TextEditingController _confirmPasswordCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Login(),
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Color(0xFF319F5F),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _emailCtrl,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: _passwordCtrl,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onLongPress: (){},
+                child: const Text('Log In',
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextButton(
+              child: const Text('Dont have an account yet? Sign Up'),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -1,11 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Signup/components/body.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignUpScreen extends StatelessWidget {
+  SignUpScreen({
+    Key key,
+  }) : super(key: key);
+
+  final TextEditingController _emailCtrl = TextEditingController();
+  final TextEditingController _passwordCtrl = TextEditingController();
+  final TextEditingController _confirmPasswordCtrl = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Body(),
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: Color(0xFF319F5F),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _emailCtrl,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: _passwordCtrl,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 15),
+            TextField(
+              controller: _confirmPasswordCtrl,
+              decoration: const InputDecoration(
+                labelText: 'Confirm Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onLongPress: (){},
+                child: const Text('Submit',
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextButton(
+              child: const Text('Already have an account? Sign In'),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
